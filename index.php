a9d6029e46bc16b42503807e975c2c2635187d9c
GO2DESTROY <GO2DESTROY>
2015-11-16 16:24:58 +0100

    include

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * @author  Maurice Schurink
  * @version 0.2
  */
-include_once './system/MS_core.php';
-include_once './system/MS_main.php';
+include './system/MS_core.php';
+include './system/MS_main.php';
 $MS_main = new system\MS_main();
 $MS_main->boot();
