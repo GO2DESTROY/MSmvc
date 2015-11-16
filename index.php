09158bb2bb8679988c1385a5edf78328bde7330b
GO2DESTROY <GO2DESTROY>
2015-11-16 16:25:26 +0100

    require

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * @author  Maurice Schurink
  * @version 0.2
  */
-include './system/MS_core.php';
-include './system/MS_main.php';
+require './system/MS_core.php';
+require './system/MS_main.php';
 $MS_main = new system\MS_main();
 $MS_main->boot();
