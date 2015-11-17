2f53d86f2f2cdc258e3035ec5c7ee38d88578d52
GO2DESTROY <GO2DESTROY>
2015-11-17 11:30:35 +0100

    update

--- public/views/example.php
+++ public/views/example.php
@@ -1,5 +1,9 @@
 <!DOCTYPE HTML>
 <html>
-<head><title>MSmvc</title></head>
+<head>
+	<title>MSmvc</title>
+	<meta charset="UTF-8">
+	<meta name="author" content="Maurice Schurink">
+</head>
 <body>Welcome</body>
 </html>
