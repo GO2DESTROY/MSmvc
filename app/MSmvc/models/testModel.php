384bc094759a7a3fedce15c36476bfe9ce2cd813
GO2DESTROY <mauriceschurink@msn.com>
2016-12-22 13:30:28 +0100

    added model to db support basic collm

--- app/MSmvc/models/testModel.php
+++ app/MSmvc/models/testModel.php
@@ -1,20 +1,28 @@
 <?php
 
 namespace MSmvc\models;
+
 use MSmvc\system\models\MS_model;
+use MSmvc\system\models\properties\integer;
 use MSmvc\system\models\properties\varchar;
 
 class testModel extends MS_model {
 	function __construct() {
-	    $this->dataBaseConnection = 'development';
+		$this->dataBaseConnection = 'development';
+		$id = new integer();
+		$id->name = "id";
+		$id->setPrimaryKey(true);
+		$id->setAutoIncrement(true);
+
+		$this->addField($id);
 		$name = new varchar();
 		$name->name = 'name';
 
 		$this->addField($name);
 
-        $test = new varchar();
-        $test->name = 'test';
+		$test = new varchar();
+		$test->name = 'test';
 
-        $this->addField($test);
+		$this->addField($test);
 	}
 }
--- app/MSmvc/system/models/properties/MS_property.php
+++ app/MSmvc/system/models/properties/MS_property.php
@@ -7,21 +7,76 @@ namespace MSmvc\system\models\properties;
  * @package system\models\properties
  */
 abstract class MS_property {
-    public $name;
-    public $length = 25;
-    public $type = 'varchar';
-    public $default = NULL;
-    public $collation;
-    public $attributes;
-    public $notNull = FALSE;
-    public $externalResourceTable = NULL;
-    protected $autoIncrement;
-    public $value;
+	public $name;
+	public $length = 25;
+	public $type = 'varchar';
+	public $default = NULL;
+	public $collation;
+	public $attributes;
+	private $notNull = FALSE;
+	public $externalResourceTable = NULL;
+	private $autoIncrement;
+	public $value;
+	private $primaryKey = false;
 
-    /**
-     * @return bool: true if the validation is correct
-     * @throws \Exception: exception of the type invalidPropertyException
-     */
-    abstract function validateProperty();
+	/**
+	 * @return boolean
+	 */
+	public function isNotNull(): bool {
+		return $this->notNull;
+	}
+
+	/**
+	 * @param boolean $notNull
+	 */
+	public function setNotNull(bool $notNull = true) {
+		if ($notNull == false) {
+			$this->primaryKey = false;
+		}
+		$this->notNull = $notNull;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isPrimaryKey(): bool {
+		return $this->primaryKey;
+	}
+
+	/**
+	 * @param boolean $primaryKey
+	 */
+	public function setPrimaryKey(bool $primaryKey = true) {
+		if ($primaryKey == true) {
+			$this->notNull = true;
+		}
+		$this->primaryKey = $primaryKey;
+	}
+
+	/**
+	 * @return bool: true if the validation is correct
+	 * @throws \Exception: exception of the type invalidPropertyException
+	 */
+	abstract public function validateProperty();
+
+	/**
+	 * @return mixed
+	 */
+	public function getAutoIncrement() {
+		return $this->autoIncrement;
+	}
+
+	/**
+	 * @param bool $autoIncrement
+	 * @return $this
+	 */
+	protected function setAutoIncrement(bool $autoIncrement = true) {
+		if ($autoIncrement === true) {
+			$this->autoIncrement = "AUTO_INCREMENT";
+		} else {
+			$this->autoIncrement = null;
+		}
+		return $this;
+	}
 
 }
