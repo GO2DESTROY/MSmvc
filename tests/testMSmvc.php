174c6448c7e46bc64563e1b999d7981fca984026
Maurice Schurink <mauriceschurink@msn.com>
2017-01-01 20:17:55 +0100

    query builder

---
+++ tests/testMSmvc.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class testMSmvc extends TestCase{
+
+    /**
+     * a php unit test that boots the whole framework
+     */
+    public function testBootingMSmvc() {
+        return TRUE;
+    }
+}
