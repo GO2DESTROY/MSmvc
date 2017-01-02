490ad230e7332c6d065f841b3a25f11fc18190e0
Maurice Schurink <mauriceschurink@msn.com>
2017-01-02 18:38:49 +0100

    Version 0.3

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
  * todo: split the router up to a setter and executer with the use of MS_requestHandler. MS_requestHandler will now set the request and the MS_router will assume it's there
  * @package MSmvc
  * @author  Maurice Schurink
- * @version 0.2
+ * @version 0.3
  */
 require __DIR__ . '/../vendor/autoload.php';
 use MSmvc\MS_start;
