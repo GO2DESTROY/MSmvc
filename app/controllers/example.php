dd37549b1d51dbe7d9ad3bd6aaad9f4986b8e03b
Maurice Schurink <mauriceschurink@msn.com>
2017-03-19 23:00:42 +0100

    filters work

--- app/controllers/example.php
+++ app/controllers/example.php
@@ -2,12 +2,15 @@
 
 namespace App\controllers;
 
+use App\models\testModel;
+use App\system\databases\MS_migrationBuilder;
 use App\system\MS_controller;
 
 class example extends MS_controller {
 
     public function index() {
-
+        $test = new MS_migrationBuilder(new testModel());
+        var_dump($test);
         echo 123;
         //  echo $test->getShortModelName();
     }
